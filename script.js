45d3b009ef9042673b6fed4eff7979fee9195a07
unknown <khalidabdi3101@gmail.com>
2024-09-01 00:07:49 +0300

    done exercise grading

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello");
+const grade = 50
+
+if(grade > 90 && grade < 100){
+    console.log("You got A grade")
+}
+
+if(grade > 75 && grade < 90){
+    console.log("You got B grade")
+}
+
+if(grade > 60 && grade < 75){
+    console.log("You got c grade")
+}
+
+
+else{
+    console.log("You got A failed")
+}
+
